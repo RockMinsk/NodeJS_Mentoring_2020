9f2d5a39c6677752f7f6d48f71d0aa593c7b31ba
Kiryl Shytsikau <kiryl.shytsikau@gmail.com>
2020-07-26 21:20:21 +0300

    Task 1.1 - read a string from the standard input stdin, reverse it and then write it to the standard output stdout

---
+++ README.md
@@ -0,0 +1,11 @@
+NodeJS Global Mentoring Program (2020Q3)
+
+## Prerequisites
+1. NODE version >= 12.18.3
+2. NPM version >= 6.14.6
+3. Git
+
+## Specific information
+- Module 1
+    - Task 1.1 - run script "npm run task-1-1" to get reversed text in console
+    - Task 1.2 - run script "npm run task-1-2" to convert csv file to txt
---
+++ task-1-1/index.js
@@ -0,0 +1,10 @@
+import * as readline from 'readline';
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    terminal: false
+});
+
+console.log('Enter text to get its reversed copy. Press CTRL + C to terminate the program.');
+rl.on('line', line => console.log([...line].reverse().join("")));
