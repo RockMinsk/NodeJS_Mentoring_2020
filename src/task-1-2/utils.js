bd1c5e7eb9f1d88a886599aa5b17603a9e9c7319
Kiryl Shytsikau <kiryl.shytsikau@gmail.com>
2020-08-03 01:48:23 +0300

    Task 1.2 - minor update for utils

--- src/task-1-2/utils.js
+++ src/task-1-2/utils.js
@@ -2,10 +2,12 @@ import { EXPECTED_CSV_FILE_COLUMNS } from './constant';
 
 export function transformJson(jsonObj) {
     for (let key in jsonObj) {
-        if (EXPECTED_CSV_FILE_COLUMNS.includes(key) && key !== key.toLowerCase()) {
+        if (EXPECTED_CSV_FILE_COLUMNS.includes(key)) {
             if (jsonObj[key] && !isNaN(jsonObj[key])) { jsonObj[key] = +jsonObj[key]; }
-            Object.defineProperty(jsonObj, key.toLowerCase(), Object.getOwnPropertyDescriptor(jsonObj, key));
-            delete jsonObj[key];
+            if (key !== key.toLowerCase()) {
+                Object.defineProperty(jsonObj, key.toLowerCase(), Object.getOwnPropertyDescriptor(jsonObj, key));
+                delete jsonObj[key];
+            }
         } else {
             delete jsonObj[key];
         }
