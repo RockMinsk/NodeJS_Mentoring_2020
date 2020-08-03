f0440529ce04ddcad7bf644a457fa8cd26caf459
Kiryl Shytsikau <kiryl.shytsikau@gmail.com>
2020-08-04 00:33:33 +0300

    Task 1.3 - fixes after code review

--- src/task-1-2/constant.js
+++ src/task-1-2/constant.js
@@ -7,8 +7,8 @@ const FILE_NAME = 'example';
 const FILE_INPUT = process.argv[2] ?? `${FILE_NAME}.csv`;
 const FILE_OUTPUT = process.argv[3] ?? `${FILE_NAME}.txt`;
 
-export const FULL_PATH_INPUT = `${PATH_TO_FOLDER_INPUT}\\${FILE_INPUT}`;
-export const FULL_PATH_OUTPUT = `${PATH_TO_FOLDER_OUTPUT}\\${FILE_OUTPUT}`;
+export const FULL_PATH_INPUT = path.join(`${PATH_TO_FOLDER_INPUT}`, `${FILE_INPUT}`);
+export const FULL_PATH_OUTPUT = path.join(`${PATH_TO_FOLDER_OUTPUT}`, `${FILE_OUTPUT}`);
 
 export const EXPECTED_CSV_FILE_COLUMNS = [
     'Book',
