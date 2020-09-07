1620657166eef97ea8f833d5f60073b27fcd1cbd
Kiryl Shytsikau <kiryl.shytsikau@gmail.com>
2020-09-08 01:15:06 +0300

    Task 2 - fix typo

--- src/users/user.routes.ts
+++ src/users/user.routes.ts
@@ -5,16 +5,16 @@ import { userSchemas, validationTarget } from './user.validation';
 
 export const userRoute = Router();
 
-const userControler = new UserController();
+const userController = new UserController();
 
 userRoute.all('/*', checkAuth);
 userRoute.all('/:id', validateSchema(userSchemas.id, validationTarget.id));
 
 userRoute.route('/')
-    .get(userControler.getUsers)
-    .post(validateSchema(userSchemas.addUser, validationTarget.addUser), userControler.addUser);
+    .get(userController.getUsers)
+    .post(validateSchema(userSchemas.addUser, validationTarget.addUser), userController.addUser);
 
 userRoute.route('/:id')
-    .get(userControler.getUserById)
-    .put(validateSchema(userSchemas.updateUser, validationTarget.updateUser), userControler.updateUser)
-    .delete(userControler.deleteUser);
+    .get(userController.getUserById)
+    .put(validateSchema(userSchemas.updateUser, validationTarget.updateUser), userController.updateUser)
+    .delete(userController.deleteUser);
